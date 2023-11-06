b20689b0e9d7ae8c331f702cd4bc7812cfded1dc
Denys Dronov <dronovdenis36@gmail.com>
2023-11-06 10:26:04 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Calculator<T>
+    {
+        public Func<T, T, T> Add { get; set; }
+        public Func<T, T, T> Subtract { get; set; }
+        public Func<T, T, T> Multiply { get; set; }
+        public Func<T, T, T> Divide { get; set; }
+
+        public Calculator()
+        {
+            
+            Add = (a, b) => (dynamic)a + b;
+            Subtract = (a, b) => (dynamic)a - b;
+            Multiply = (a, b) => (dynamic)a * b;
+            Divide = (a, b) => (dynamic)a / b;
+        }
+
+        public T PerformOperation(T a, T b, Func<T, T, T> operation)
+        {
+            return operation(a, b);
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp9;
+
+static void Main()
+{
+    Calculator<int> intCalculator = new Calculator<int>();
+    Calculator<double> doubleCalculator = new Calculator<double>();
+
+    int resultIntAdd = intCalculator.PerformOperation(5, 3, intCalculator.Add);
+    int resultIntSubtract = intCalculator.PerformOperation(5, 3, intCalculator.Subtract);
+    double resultDoubleMultiply = doubleCalculator.PerformOperation(5.0, 3.0, doubleCalculator.Multiply);
+    double resultDoubleDivide = doubleCalculator.PerformOperation(10.0, 2.0, doubleCalculator.Divide);
+
+    Console.WriteLine($"Integer Addition: {resultIntAdd}");
+    Console.WriteLine($"Integer Subtraction: {resultIntSubtract}");
+    Console.WriteLine($"Double Multiplication: {resultDoubleMultiply}");
+    Console.WriteLine($"Double Division: {resultDoubleDivide}");
+}
