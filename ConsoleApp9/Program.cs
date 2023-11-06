32a338146db91bd4758e59e0f4792b7741377dee
Denys Dronov <dronovdenis36@gmail.com>
2023-11-06 11:22:11 +0200

    1

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -1,17 +1,19 @@
 ﻿using ConsoleApp9;
-
-static void Main()
+class Program
 {
-    Calculator<int> intCalculator = new Calculator<int>();
-    Calculator<double> doubleCalculator = new Calculator<double>();
+    static void Main()
+    {
+        Calculator<int> intCalculator = new Calculator<int>();
+        Calculator<double> doubleCalculator = new Calculator<double>();
 
-    int resultIntAdd = intCalculator.PerformOperation(5, 3, intCalculator.Add);
-    int resultIntSubtract = intCalculator.PerformOperation(5, 3, intCalculator.Subtract);
-    double resultDoubleMultiply = doubleCalculator.PerformOperation(5.0, 3.0, doubleCalculator.Multiply);
-    double resultDoubleDivide = doubleCalculator.PerformOperation(10.0, 2.0, doubleCalculator.Divide);
+        int resultIntAdd = intCalculator.PerformOperation(5, 3, intCalculator.Add);
+        int resultIntSubtract = intCalculator.PerformOperation(5, 3, intCalculator.Subtract);
+        double resultDoubleMultiply = doubleCalculator.PerformOperation(5.0, 3.0, doubleCalculator.Multiply);
+        double resultDoubleDivide = doubleCalculator.PerformOperation(10.0, 2.0, doubleCalculator.Divide);
 
-    Console.WriteLine($"Integer Addition: {resultIntAdd}");
-    Console.WriteLine($"Integer Subtraction: {resultIntSubtract}");
-    Console.WriteLine($"Double Multiplication: {resultDoubleMultiply}");
-    Console.WriteLine($"Double Division: {resultDoubleDivide}");
+        Console.WriteLine($"Integer Addition: {resultIntAdd}");
+        Console.WriteLine($"Integer Subtraction: {resultIntSubtract}");
+        Console.WriteLine($"Double Multiplication: {resultDoubleMultiply}");
+        Console.WriteLine($"Double Division: {resultDoubleDivide}");
+    }
 }
